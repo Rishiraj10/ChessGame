4b15bb4264eef44d364a32be9f6a1c8b3a0283d8
Rishiraj10 <rkystore4@gmail.com>
2025-06-14 20:48:57 +0530

    Working on chess logic implementation

--- src/Chess.kt
+++ src/Chess.kt
@@ -1,2 +1,47 @@
 import korlibs.datastructure.*
 
+enum class ChessPieceKind(val key: Char, val index: Int) {
+    PAWN('P',0),
+    BISHOP('B', 1),
+    KNIGHT('K',2),
+    ROOK('R',3),
+    QUEEN('Q',4),
+    KING('N', 5)
+}
+
+enum class ChessPlayer {
+    BLACK, WHITE
+}
+
+sealed interface ChessCell {
+    val key: Char
+
+    object Empty: ChessCell{
+        override val key: Char get() = '.'
+        override fun toString(): String = "$key"
+    }
+    data class Piece(
+        val kind: ChessPieceKind,
+        val player: ChessPlayer
+    ): ChessCell {
+        override val key: Char get() = when (player) {
+            ChessPlayer.BLACK -> kind.key.uppercaseChar()
+            ChessPlayer.WHITE -> kind.key.lowercaseChar()
+        }
+        override fun toString(): String = "$key"
+    }
+}
+
+
+
+class ChessBoard(val array : Array2<ChessCell> = Array2<ChessCell>(8, 8) {ChessCell.Empty}) {
+    override fun toString(): String {
+       return (0 until array.height).joinToString("\n") { y ->
+            (0 until array.width).joinToString("") { x ->
+                array[x, y].toString()
+            }
+
+        }
+
+    }
+}
--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,12 @@
+import korlibs.datastructure.*
+import korlibs.image.bitmap.*
 import korlibs.korge.*
 import korlibs.korge.scene.*
 import korlibs.korge.view.*
 import korlibs.image.color.*
 import korlibs.math.*
 import korlibs.math.geom.*
+import korlibs.math.geom.slice.*
 
 suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors["#2b2b2b"]) {
 	val sceneContainer = sceneContainer()
@@ -16,15 +19,15 @@ class MyScene : PixelatedScene(128*8, 128*8, sceneSmoothing = true) {
         val images = __KR.KRGfx.chessShadow.read()
         val blacks = images.run{ listOf(
             b_pawn, b_bishop,
-            b_knight, b_knight,
-            b_rook, b_queen,
+            b_knight, b_rook,
+            b_queen,
             b_king
         )
         }
         val whites = images.run{ listOf(
             w_pawn, w_bishop,
-            w_knight, w_knight,
-            w_rook, w_queen,
+            w_knight, w_rook,
+            w_queen,
             w_king
         )
         }
@@ -34,12 +37,42 @@ class MyScene : PixelatedScene(128*8, 128*8, sceneSmoothing = true) {
                 image(bgs[(x+y).isOdd.toInt()]).xy(128*x, 128*y)
             }
         }
-        for (n in 0 until 6){
-            image(blacks[n]).centered.xy(n*128 + 64, 0+64).scale(0.9)
-            image(whites[n]).centered.xy(n*128 + 64, 128+64).scale(0.9)
+
+        val board = ChessBoard()
+        board.array[0, 0] = ChessCell.Piece(ChessPieceKind.BISHOP, ChessPlayer.BLACK)
+        board.array[1, 0] = ChessCell.Piece(ChessPieceKind.KNIGHT, ChessPlayer.WHITE)
+        board.array[6, 6] = ChessCell.Piece(ChessPieceKind.KING, ChessPlayer.BLACK)
+
+        representBoard(board,blacks, whites)
+        //for (n in 0 until 6){
+         //   image(blacks[n]).centered.xy(n*128 + 64, 0+64).scale(0.9)
+         //   image(whites[n]).xy(n*128 + 64, 128)
+      //  }
+
+    }
+
+    private fun Container.representBoard(
+        board: ChessBoard = ChessBoard(),
+        blacks: List<RectSlice<out Bitmap>>,
+        whites: List<RectSlice<out Bitmap>>
+    ) {
 
 
+        fun ChessCell.getImage(): BmpSlice {
+            return when (this) {
+                ChessCell.Empty -> Bitmaps.transparent
+                is ChessCell.Piece -> {
+                    val array = when (this.player) {
+                        ChessPlayer.BLACK -> blacks
+                        ChessPlayer.WHITE -> whites
+                    }
+                    array[this.kind.index]
+                }
+            }
         }
 
+        board.array.each { x, y, v ->
+            image(v.getImage()).centered.xy(x * 128 + 64, y * 128 + 64).scale(0.9)
+        }
     }
 }
